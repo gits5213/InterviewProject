258c06c234675bdc4276eab6f90b10ccfcaf2e70
Shahnewaz <mshahnewaz1@gmail.com>
2017-01-20 23:17:44 -0500

    Update FizzBuzz.java

--- src/PersonalPactice/FizzBuzz.java
+++ src/PersonalPactice/FizzBuzz.java
@@ -8,15 +8,14 @@ public class FizBuz {
 
 	public static void main(String[] args) {
 		
-		
 		for (int i=1; i<100; i++){
 			
 		if (i%15==0)
-				System.out.println("FizzBuzz");
+			System.out.println("FizzBuzz");
 		else if(i%3==0)
-				System.out.println("Fizz");
+			System.out.println("Fizz");
 		else if (i%5==0)
-				System.out.println("Buzz");
+			System.out.println("Buzz");
 		else
 			System.out.println(i);
 	
