621b5e3890745965997d1ecf434c6c968d9215c4
Md Zaman <md83shahnewaz@gmail.com>
2020-11-26 14:32:02 -0500

    Dropdown List matched with actual and Expected

--- take-home-webdriver-test/src/test/java/testCases/Dropdown_6.java
+++ take-home-webdriver-test/src/test/java/testCases/Dropdown_6.java
@@ -1,6 +1,9 @@
 package testCases;
 
 import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
@@ -17,10 +20,32 @@ public class Dropdown_6 extends BaseTest {
 		sleepTest(1000);
 		
 		Select select = new Select(dp.getDropDown());
+		
 		select.selectByIndex(1);
 		WebElement option = select.getFirstSelectedOption();
 		String selectedValueInDropDown = option.getText();
 		System.out.println(selectedValueInDropDown);
 		assertEquals(Utils.FIRST_SELECTED_VALUE_IN_DROPDOWN, selectedValueInDropDown);	
-	}
+	
+		//Print all the value form the Drop-down list
+		dp.getElementText();
+		
+		//Validate Drop-down list Actual and Expected
+		int count = 0;
+	    String[] dropDownList = {"Please select an option", "Option 1", "Option 2"};
+		
+		List<WebElement> options = select.getOptions();
+	    for (WebElement we : options) {
+	        for (int i = 0; i < dropDownList.length; i++) {
+	            if (we.getText().equals(dropDownList[i])) {
+	                count++;
+	            }
+	        }
+	    }
+	    if (count == dropDownList.length) {
+	        System.out.println("Dropdown list matched! :)");
+	    } else {
+	        System.out.println("Dropdown list doesn't matched!");
+	    }
+	}	
 }
--- take-home-webdriver-test/src/test/java/testPages/DropdownPage.java
+++ take-home-webdriver-test/src/test/java/testPages/DropdownPage.java
@@ -1,5 +1,7 @@
 package testPages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,5 +17,18 @@ public class DropdownPage extends BasePage{
 	
 	public WebElement getDropDown() {
 		return dropDown;
+	}
+	
+	@FindBy(css = "#content div select#dropdown")
+	public List<WebElement> dropDownList;
+	
+	public List<WebElement> getDropDownList() {
+		return dropDownList;
+	}
+	
+	public void getElementText(){
+	      for (WebElement objElement : dropDownList) {
+	        System.out.println(objElement.getText());
+	    }
 	}
 }
