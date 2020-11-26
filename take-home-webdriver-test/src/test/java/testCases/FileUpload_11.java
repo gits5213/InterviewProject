c81aa45afcc3b90d95c7ff9d25d3562f67d1d69f
Md Zaman <md83shahnewaz@gmail.com>
2020-11-26 09:38:53 -0500

    Automated File Upload

--- take-home-webdriver-test/src/test/java/testCases/FileUpload_11.java
+++ take-home-webdriver-test/src/test/java/testCases/FileUpload_11.java
@@ -1,7 +1,5 @@
 package testCases;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -10,32 +8,27 @@ import testPages.FileUploadingPage;
 import utilities.Utils;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class FileUpload_11 extends BaseTest {
 	
 	public WebDriverWait wait;
 	
 	@Test
-	public void dynamicLoading_9(){
+	public void fileUpload_11(){
 		
 		String baseDir = System.getProperty("user.dir");
 		String imageDir = baseDir + "/src/test/java/resource/logo.png";
 		System.out.println(imageDir);
 		
-		//FileUploadingPage fl = new FileUploadingPage(driver);
-		driver.get("http://the-internet.herokuapp.com/upload");
+		FileUploadingPage fl = new FileUploadingPage(driver);
+		driver.get(Utils.BASE_URLS + "/upload");
 		sleepTest(5000);
-//		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector("#file-submit")).click();
-		//fl.getChoseFile().click();
+		WebDriverWait wait = new WebDriverWait(driver,60);
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#file-upload")));
+		fl.getChoseFile().click();
+		uploadFileWithRobot(imageDir);
+		fl.getUploadFile().click();
 		sleepTest(5000);
-//		uploadFileWithRobot(imageDir);
-//		sleepTest(1000);
-//		fl.getUploadFile().click();
-//		Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#uploaded-files"))).getText().equals("logo.png"));
-//		sleepTest(5000);
-	
+		Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#uploaded-files"))).getText().equals("logo.png"));
+		sleepTest(5000);
 	}
-	
-
 }
